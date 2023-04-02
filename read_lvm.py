aad179d8fed80062467cc2adde4743361986e107
Jeff Bowman <jsbowman@ucsd.edu>
2023-04-02 09:06:39 -0700

    Update read_lvm.py

--- read_lvm.py
+++ read_lvm.py
@@ -182,51 +182,63 @@ sccoos_temp.index = pd.to_datetime(sccoos_temp.index, format = '%Y-%m-%dT%H:%M:%
 
 col_str = ["time", "ms", "Water", "N2", "O2", "Ar", "Inlet Temperature", "Vacuum Pressure"]
 
-## Iterate across all csv files parse, adding to list.
+## Iterate across all csv files parse, adding to list. Start by reading the old
+## combined data file, and create a new one if not present.
 
-old_frame = pd.read_csv('MIMS_data_vol2.csv.gz')
+try:
+    old_frame = pd.read_csv('MIMS_data_vol2.csv.gz', index_col = 0)
+except FileNotFoundError:
+    old_frame = pd.DataFrame(columns = col_str)
+    old_frame['source_file'] = []
+
 old_frame['time'] = pd.to_datetime(old_frame['time'], format = '%Y-%m-%d %H:%M:%S')
 li = [old_frame]
 
-for filename in csv_files:
+## Iterate across the csv files, but don't include the most recent because
+## that creates problems for the instrument software.
+
+for filename in csv_files[0:-1]:
+    
+    if filename not in old_frame.source_file:
     
     ## Try clause added because Massoft occasionally starts exporting wrong number of columns
     ## and this needs to be fixed manually.  The first error this will raise is ValueError
     ## when pandas fails to parse date string.
     
-    try:
-    
-        with open(filename, 'r') as file_in:
-            for line in file_in.readlines():
-                if line.startswith('\"Date\"'):
-                    line = line.strip()
-                    line = line.split(',')
-                    date = line[1]
-                    time = line[3]
-                    date_time_0 = ' '.join([date, time])
-                    date_time_0 = pd.to_datetime(date_time_0, format = '%m/%d/%Y %I:%M:%S %p')
-                    break
-                                    
-        if development == True:
-            base_name = filename.split('\\')[-1]
-        else:
-            base_name = filename.split('/')[-1]
+        try:
         
-        df = pd.read_csv(filename, skiprows=30, header=0, names = col_str, index_col = False)
-        df['elapsed_time'] = df['time']
+            with open(filename, 'r') as file_in:
+                for line in file_in.readlines():
+                    if line.startswith('\"Date\"'):
+                        line = line.strip()
+                        line = line.split(',')
+                        date = line[1]
+                        time = line[3]
+                        date_time_0 = ' '.join([date, time])
+                        date_time_0 = pd.to_datetime(date_time_0, format = '%m/%d/%Y %I:%M:%S %p')
+                        break
+                                        
+            if development == True:
+                base_name = filename.split('\\')[-1]
+            else:
+                base_name = filename.split('/')[-1]
+            
+            df = pd.read_csv(filename, skiprows=30, header=0, names = col_str, index_col = False)
+            df['elapsed_time'] = df['time']
+            
+            for index, row in df.iterrows():
+                time_delta = list(map(int, row['time'].split(':')))
+                df.loc[index, 'time'] = date_time_0 + timedelta(hours = time_delta[0], minutes = time_delta[1], seconds = time_delta[2])
         
-        for index, row in df.iterrows():
-            time_delta = list(map(int, row['time'].split(':')))
-            df.loc[index, 'time'] = date_time_0 + timedelta(hours = time_delta[0], minutes = time_delta[1], seconds = time_delta[2])
-    
-        #df['time'] = pd.to_datetime(df['time'], format = '%m/%d/%Y %I:%M:%S %p', exact = True)
-        df['source_file'] = base_name
-        df['start_time'] = date_time_0
-          
-        li.append(df)
-        
-    except ValueError:
-        continue
+            #df['time'] = pd.to_datetime(df['time'], format = '%m/%d/%Y %I:%M:%S %p', exact = True)
+            df['source_file'] = base_name
+            df['start_time'] = date_time_0
+              
+            li.append(df)
+            print('adding', base_name)
+            
+        except ValueError:
+            continue
     
 ## Concatenate individual dataframes to master frame and sort by date.
 
@@ -251,7 +263,7 @@ sccoos_temp_round['O2:Ar_sat'] = sccoos_temp_round['O2_sat'] / sccoos_temp_round
 if use_sccoos == True:
     sort_round = sort[['O2', 'O2:Ar', 'N2:Ar']]
     sort_round.index = sort.time
-    sort_round['date_time'] = sort_round.index.round('5T')
+    sort_round.loc[sort_round.index, 'date_time'] = sort_round.index.round('5T')
     sort_round = sort_round.groupby(sort_round.date_time).mean()
     
     edna_mims_round = pd.concat([sccoos_temp_round, sort_round], axis = 1, join = 'inner')
@@ -317,7 +329,7 @@ pio.write_html(fig, file= 'ecoobs/' + 'O2_bio' + ".html", auto_open=False)
 
 mims_col_filter = sort['N2:Ar'] > 0
 #mims_col_filter = (sort['N2:Ar'] > 9) & (sort['N2:Ar'] < 20)
-#mims_col_filter[0:-20000] = False
+mims_col_filter[0:-20000] = False
             
 for col in ['O2', 'Ar', 'Inlet Temperature', 'Vacuum Pressure', 'N2','O2:Ar', 'N2:Ar', 'Water']:
     
